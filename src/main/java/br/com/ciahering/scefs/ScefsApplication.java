25fd38fdc23631a11ad6f43955e66887ed9400dd
josinaldorrgomes <josinaldorobertorocha@gmail.com>
2022-08-09 13:09:03 -0300

    Projeto Spring Boot versionado

---
+++ src/main/java/br/com/ciahering/scefs/ScefsApplication.java
@@ -0,0 +1,13 @@
+package br.com.ciahering.scefs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScefsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScefsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ciahering/scefs/ScefsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ciahering.scefs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScefsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
